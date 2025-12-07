8117a2cad8485ebbf09f54ad4e35e965333bfa15
Arthur <164928663+rth-ur@users.noreply.github.com>
2025-12-06 21:50:09 -0300

    Update schema references to 'biblioteca' in SQL file

--- Roles/roles-admins-acoes.sql
+++ Roles/roles-admins-acoes.sql
@@ -6,23 +6,23 @@ GRANT ALL PRIVILEGES ON SCHEMA public TO admins;
 
 -- Permissões totais em todas as tabelas existentes
 GRANT ALL PRIVILEGES ON 
-    tb01_leitor,
-    tb02_autor,
-    tb03_livro,
-    tb04_emprestimo,
-    tb05_multa
+    biblioteca.tb01_leitor,
+    biblioteca.tb02_autor,
+    biblioteca.tb03_livro,
+    biblioteca.tb04_emprestimo,
+    biblioteca.tb05_multa
 TO admins;
 
 -- Permissão total para futuras tabelas criadas automaticamente
-ALTER DEFAULT PRIVILEGES IN SCHEMA Biblioteca
+ALTER DEFAULT PRIVILEGES IN SCHEMA biblioteca
 GRANT ALL PRIVILEGES ON TABLES TO admins;
 
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA Biblioteca TO admins;
-ALTER DEFAULT PRIVILEGES IN SCHEMA Biblioteca
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA biblioteca TO admins;
+ALTER DEFAULT PRIVILEGES IN SCHEMA biblioteca
 GRANT ALL PRIVILEGES ON SEQUENCES TO admins;
 
 -- Permissões totais em funções e procedures
-GRANT EXECUTE ON PROCEDURE sp_registrar_emprestimo(INT, INT, DATE) TO admins;
+GRANT EXECUTE ON PROCEDURE biblioteca.sp_registrar_emprestimo(INT, INT, DATE) TO admins;
 
-GRANT EXECUTE ON FUNCTION fn_trg_registrar_multa() TO admins;
-GRANT EXECUTE ON FUNCTION fn_calcular_multa_atraso(INT) TO admins;
+GRANT EXECUTE ON FUNCTION biblioteca.fn_trg_registrar_multa() TO admins;
+GRANT EXECUTE ON FUNCTION biblioteca.fn_calcular_multa_atraso(INT) TO admins;
